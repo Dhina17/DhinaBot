dc21ffac39eb54019e5afa85db41f7238fb96476
Dhina17 <dhinalogu@gmail.com>
2021-04-06 21:31:35 +0530

    config: Make the USE_TEAM_DRIVE optional
    - before it causes NPE if we don't set USE_TEAM_DRIVE env variable

--- src/main/java/io/github/dhina17/tgbot/configs/GdriveConfig.java
+++ src/main/java/io/github/dhina17/tgbot/configs/GdriveConfig.java
@@ -17,17 +17,19 @@
 
 package io.github.dhina17.tgbot.configs;
 
+import java.util.Optional;
+
 public class GdriveConfig {
 
     /**
-     * Set true if you want to use Team drive
+     * Set true if you want to use Team drive (Optional)
      */
-    public static final Boolean USE_TEAM_DRIVE = Boolean.parseBoolean(
+    public static final Boolean USE_TEAM_DRIVE = Optional.ofNullable(Boolean.parseBoolean(
                                                                         System.getenv("USE_TEAM_DRIVE")
-                                                                    );
+                                                                    )).orElse(false);
 
     /*
-     * Place your TeamDrive ID here
+     * Place your TeamDrive ID here (Optional)
      */
 
     public static final String TEAM_DRIVE_ID = System.getenv("TEAM_DRIVE_ID");
